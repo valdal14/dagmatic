68ad7934c480656dd389c38f7b7f7fa88a79c093
Valerio DAlessio <u105550@canda.com>
2026-04-03 08:36:28 +0200

    feature: remove dep duplication from the test file and perform linting and formatting

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,9 +1,8 @@
 from dagmatic.core.task import Task
 
-from dagmatic.core.task import Task
 
 def test_task_init():
-    t = Task(id="extract_data") 
-    
+    t = Task(id="extract_data")
+
     assert t is not None
     assert t.id == "extract_data"
