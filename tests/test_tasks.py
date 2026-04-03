9fe693024ed7b606b9030fd701d9afff10d8ab36
Valerio DAlessio <u105550@canda.com>
2026-04-03 08:32:40 +0200

    feature: add Task class and verified that the class can be instanciated

---
+++ tests/test_tasks.py
@@ -0,0 +1,9 @@
+from dagmatic.core.task import Task
+
+from dagmatic.core.task import Task
+
+def test_task_init():
+    t = Task(id="extract_data") 
+    
+    assert t is not None
+    assert t.id == "extract_data"
